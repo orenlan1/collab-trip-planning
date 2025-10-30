820feffa80e3ef0181c1d3ec81370addf6aaf457
orenlan1 <128816521+orenlan1@users.noreply.github.com>
2025-10-30 10:10:31 +0200

    Delete server/dist directory

--- server/dist/auth/strategies/google.d.ts
+++
@@ -1,3 +0,0 @@
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-export declare const googleStrategy: GoogleStrategy;
-//# sourceMappingURL=google.d.ts.map
--- server/dist/auth/strategies/local.d.ts
+++
@@ -1,3 +0,0 @@
-import { Strategy as LocalStrategy } from 'passport-local';
-export declare const localStrategy: LocalStrategy;
-//# sourceMappingURL=local.d.ts.map
