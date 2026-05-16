2b3689ff055eae259e0c0f85fd6360d55e44add7
oren lan <lanoren1@gmail.com>
2026-05-16 19:49:27 +0300

    delete unused options from ChooseActivityTypeDialog

--- client/src/pages/tripday/components/ChooseActivityTypeDialog.tsx
+++ client/src/pages/tripday/components/ChooseActivityTypeDialog.tsx
@@ -1,9 +1,8 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
-import { FaPlane } from "react-icons/fa";
 import { PiForkKnifeFill } from "react-icons/pi";
 import { FaCamera } from "react-icons/fa6";
 
-export type ActivityType = 'TRANSPORTATION' | 'ACCOMMODATION' | 'DINING' | 'ACTIVITIES' | 'MISCELLANEOUS';
+export type ActivityType = 'DINING' | 'ACTIVITIES';
 
 interface AddActivityDialogProps {
   open: boolean;
@@ -24,17 +23,7 @@ export function ChooseActivityTypeDialog({ open, onOpenChange, onActivityTypeSel
         <DialogHeader>
             <DialogTitle>Choose Activity Type</DialogTitle>
         </DialogHeader>
-            <div className='grid grid-cols-3 gap-4 p-4'>
-                <button
-                  onClick={() => handleActivityTypeClick('TRANSPORTATION')}
-                  className='flex flex-col items-center p-4 rounded-lg cursor-pointer group'
-                  type="button"
-                >
-                    <div className='mb-2 bg-gray-300 p-2 rounded-xl transition-all duration-300 group-hover:bg-blue-500 group-hover:scale-110'>
-                        <FaPlane className='w-5 h-5 text-slate-500 transition-all duration-300 group-hover:text-white group-hover:scale-110' />
-                    </div>
-                    <div className="text-sm">Transportation</div>
-                </button>
+            <div className='grid grid-cols-2 gap-4 p-4'>
                 <button
                   onClick={() => handleActivityTypeClick('DINING')}
                   className='flex flex-col items-center p-4 rounded-lg cursor-pointer group'
